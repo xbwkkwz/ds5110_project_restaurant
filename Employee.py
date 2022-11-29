45ecb291b903391561fcf1062aac0ac1da669e96
xbwkkwz <xbwkk@hotmail.com>
2022-11-29 01:58:44 -0500

    modify codes

--- Employee.py
+++ Employee.py
@@ -15,10 +15,13 @@ class Employee:
     Chef can do: design menu, add ingredients, specify ingredient with dish,
     purchase ingredients.
     '''
+
+    # done
     def __init__(self):
         self.conn = None
         self.__sql_connect()
 
+    # done
     def __sql_connect(self):
         try:
             self.conn = connect(
@@ -30,22 +33,28 @@ class Employee:
         except Error as e:
             print(e)
 
-    def sql_disconnect(self):
+    # done, disconnect the sql 
+    def exit_system(self):
         self.conn.close()
         self.conn = None
 
+
+    ###########################
+    # procedure call related methods
+    ###########################
+
     # done, use this if insert, update, delete database
-    def modify_database(self, procedure_name: str, args: tuple):
+    def modify_database(self, procedure_name: str, args: tuple) -> tuple:
         with self.conn.cursor() as cursor:
             cursor.callproc(procedure_name, args)
             self.conn.commit()
             tables = cursor.stored_results()
         for table in tables:
             for row in table.fetchall():
-                return row[0]
+                return row
 
     # done, use this if read database only
-    def read_database(self, procedure_name: str, args: tuple, col: list):
+    def read_database(self, procedure_name: str, args: tuple) -> list:
         with self.conn.cursor() as cursor:
             if args:
                 cursor.callproc(procedure_name, args)
@@ -53,9 +62,13 @@ class Employee:
                 cursor.callproc(procedure_name)
             tables = cursor.stored_results()
         for table in tables:
-            df = pd.DataFrame(table.fetchall(), columns = col)
-            df.index = df.index + 1
-            print(df)
+            return table.fetchall()
+
+    # done, output a formatted data
+    def print_database(self, table: list, col: list):
+        df = pd.DataFrame(table, columns = col)
+        df.index = df.index + 1
+        print(df)
 
 
     ###########################
@@ -64,52 +77,57 @@ class Employee:
 
     # done
     def view_all_orders(self):
+        table = self.read_database("employee_view_order_history", None)
+        if not table:
+            print("No history.")
+            return
         col = ["Order ID", "Date", "Status", "In Queue", "Num Of Dish", "Subtotal", "Tips", "Total", "Customer ID", "Table ID"]
-        self.read_database("employee_view_order_history", None, col)
+        self.print_database(table, col)
 
     # done
     def view_order_detail(self, orderID: int):
         # this one should be the same as the method in customer
+        table = self.read_database("view_order_detail", (orderID,))
         col = ["Dish ID", "Name", "Quantity", "Price", "Subtotal"]
-        self.read_database("view_order_detail", (orderID,), col)
+        self.print_database(table, col)
 
     # done
     def assign_table(self, orderID: int, tableID: int):
-        message = self.modify_database("assign_table", (orderID, tableID))
-        print(message)
+        row = self.modify_database("assign_table", (orderID, tableID))
+        print(row[0])
 
     # done
     def cancel_order(self, orderID: int):
         # this one should be the same as the method in customer
-        message = self.modify_database("update_order_status", (orderID, "Canceled"))
-        print(message)
+        row = self.modify_database("update_order_status", (orderID, "Canceled"))
+        print(row[0])
 
     # done
     def update_order(self, orderID: int):
         # turn from "Received" to "Ready"
-        message = self.modify_database("update_order_status", (orderID, "Ready"))
-        print(message)
+        row = self.modify_database("update_order_status", (orderID, "Ready"))
+        print(row[0])
         
     # done
     def update_tips(self, orderID: int, tips: float):
         # this will change total cost also
-        message = self.modify_database("update_tips", (orderID, tips))
-        print(message)
+        row = self.modify_database("update_tips", (orderID, tips))
+        print(row[0])
 
     # done
     def create_order_queue(self, orderID: int, employeeID: int):
         # need to trigger order in queue
         # need to trigger subtract ingredient stock
-        message = self.modify_database("create_order_queue", (orderID, employeeID))
-        print(message)
+        row = self.modify_database("create_order_queue", (orderID, employeeID))
+        print(row[0])
         # done by the trigger "update_queue_status"
         # done by the trigger "subtract_ingredient_stock"
 
     # done
     def view_queue(self):
+        table = self.read_database("view_queue", None)
         col = ["Queue ID", "Order ID", "Order Time", "Employee ID", "Chef Name", "Status"]
-        self.read_database("view_queue", None, col)
-
+        self.print_database(table, col)
 
     
     ###########################
@@ -118,31 +136,32 @@ class Employee:
 
     # done
     def view_all_reservations(self):
+        table = self.read_database("view_all_reservations", None)
         col = ["Date", "Time", "Table ID", "Customer ID", "Customer Name", "Num Of People", "Status", "Waiter ID", "Waiter Name"]
-        self.read_database("view_all_reservations", None, col)
+        self.print_database(table, col)
 
     # done
     def assign_waiter(self, date: str, time: str, tableID: int, employeeID: int):
-        message = self.modify_database("assign_waiter", (date, time, tableID, employeeID))
-        print(message)
+        row = self.modify_database("assign_waiter", (date, time, tableID, employeeID))
+        print(row[0])
 
     # done
     def cancel_reservation(self, date: str, time: int, tableID: int):
         # this one should be the same as the method in customer
-        message = self.modify_database("cancel_reservation", (date, time, tableID))
-        print(message)
+        row = self.modify_database("cancel_reservation", (date, time, tableID))
+        print(row[0])
 
     # done
     def view_all_waiting_list(self):
+        table = self.read_database("view_all_waiting_list", None)
         col = ["Waiting ID", "Date", "Time", "Customer ID", "Customer Name", "Num Of People", "Status"]
-        self.read_database("view_all_waiting_list", None, col)
+        self.print_database(table, col)
 
     # done
     def cancel_waiting_list(self, waitingID: int):
         # this one should be the same as the method in customer
-        message = self.modify_database("cancel_waiting_list", (waitingID,))
-        print(message)
-
+        row = self.modify_database("cancel_waiting_list", (waitingID,))
+        print(row[0])
 
 
     ###########################
@@ -151,64 +170,68 @@ class Employee:
 
     # done
     def view_menu(self):
+        table = self.read_database("employee_view_menu", None)
         col = ["Category", "Dish ID", "Dish Name", "Description", "Price", "Status"]
-        self.read_database("employee_view_menu", None, col)
+        self.print_database(table, col)
 
     # done
     def add_new_category(self, categoryName: str):
-        message = self.modify_database("add_new_category", (categoryName,))
-        print(message)
+        row = self.modify_database("add_new_category", (categoryName,))
+        print(row[0])
     
     # done
     def add_new_dish(self, categoryName: str, dishName: str, price: float):
-        message = self.modify_database("add_new_dish", (categoryName, dishName, price))
-        print(message)
+        row = self.modify_database("add_new_dish", (categoryName, dishName, price))
+        print(row[0])
 
     # done
     def update_dish_status(self, menuID: int, dishStatus: int):
-        message = self.modify_database("update_dish_status", (menuID, dishStatus))
-        print(message)
+        row = self.modify_database("update_dish_status", (menuID, dishStatus))
+        print(row[0])
 
     # done
     def update_dish_price(self, menuID: int, newPrice: float):
-        message = self.modify_database("update_dish_price", (menuID, newPrice))
-        print(message)
+        row = self.modify_database("update_dish_price", (menuID, newPrice))
+        print(row[0])
 
     # done
     def update_dish_description(self, menuID: int, newDes: str):
-        message = self.modify_database("update_dish_description", (menuID, newDes))
-        print(message)
+        row = self.modify_database("update_dish_description", (menuID, newDes))
+        print(row[0])
 
     # done
     def view_ingredient_stock(self):
+        table = self.read_database("view_ingredient_stock", None)
         col = ["Ingredient ID", "Ingredient Name", "Current Stock"]
-        self.read_database("view_ingredient_stock", None, col)
+        self.print_database(table, col)
 
     # done
     def add_new_ingredient(self, ingredientName: str):
-        message = self.modify_database("add_new_ingredient", (ingredientName,))
-        print(message)
+        row = self.modify_database("add_new_ingredient", (ingredientName,))
+        print(row[0])
 
     # done
     def view_dish_ingredient(self, menuID: int):
+        table = self.read_database("view_dish_ingredient", (menuID,))
         col = ["Ingredient ID", "Ingredient Name", "Quantity"]
-        self.read_database("view_dish_ingredient", (menuID,), col)
+        self.print_database(table, col)
 
     # done
     def add_dish_has_ingredient(self, dishName: str, ingredientName: str, quantity: int):
-        message = self.modify_database("add_dish_has_ingredient", (dishName, ingredientName, quantity))
-        print(message)
+        row = self.modify_database("add_dish_has_ingredient", (dishName, ingredientName, quantity))
+        print(row[0])
 
     # done
     def view_inventory(self):
+        table = self.read_database("view_inventory", None)
         col = ["Inventory ID", "Ingredient Name", "Quantity", "Cost", "Employee ID", "Employee Name", "Purchase Date", "Exp Date"]
-        self.read_database("view_inventory", None, col)
+        self.print_database(table, col)
 
     # done
     def add_inventory(self, ingredientName: str, quantity: int, totalCost: float, employeeID: int, purchaseDate: str, expDate: str):
         # this will trigger the stock column update in ingredient
-        message = self.modify_database("add_inventory", (ingredientName, quantity, totalCost, employeeID, purchaseDate, expDate))
-        print(message)
+        row = self.modify_database("add_inventory", (ingredientName, quantity, totalCost, employeeID, purchaseDate, expDate))
+        print(row[0])
         # done through the trigger add_ingredient_stock
 
 
@@ -218,34 +241,36 @@ class Employee:
 
     # done
     def view_all_business_hours(self):
+        table = self.read_database("view_all_business_hours", None)
         col = ["Time ID", "Day", "Open", "Close", "Status"]
-        self.read_database("view_all_business_hours", None, col)
+        self.print_database(table, col)
 
     # done
     def add_business_hour(self, businessDay: str, openTime: str, closeTime: str):
-        message = self.modify_database("add_business_hour", (businessDay, openTime, closeTime))
-        print(message)
+        row = self.modify_database("add_business_hour", (businessDay, openTime, closeTime))
+        print(row[0])
 
     # done
     def update_business_hour(self, timeID: int, dayStatus: int):
         # this will also update the reservation windows automatically
-        message = self.modify_database("update_business_hour", (timeID, dayStatus))
-        print(message)
+        row = self.modify_database("update_business_hour", (timeID, dayStatus))
+        print(row[0])
 
     # done
     def view_all_tables(self):
+        table = self.read_database("view_all_tables", None)
         col = ["Table ID", "Capacity", "Status"]
-        self.read_database("view_all_tables", None, col)
+        self.print_database(table, col)
 
     # done
     def add_table(self, capacity: int):
-        message = self.modify_database("add_table", (capacity,))
-        print(message)
+        row = self.modify_database("add_table", (capacity,))
+        print(row[0])
 
     # done
     def update_table(self, tableID: int, tableStatus: int):
-        message = self.modify_database("update_table", (tableID, tableStatus))
-        print(message)
+        row = self.modify_database("update_table", (tableID, tableStatus))
+        print(row[0])
 
 
     ###########################
